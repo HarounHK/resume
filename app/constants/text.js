9ec65ab084111f78a9b6d2508cf1cb6284371ab9
Haroun Kassouri <harounkassouri368@gmail.com>
2024-09-11 05:41:24 +0100

    Work Experience sample text added with sample images

--- app/constants/text.js
+++ app/constants/text.js
@@ -3,6 +3,11 @@ import sample2 from "../../public/sample2.jpeg";
 import sample3 from "../../public/sample3.jpeg";
 import sample4 from "../../public/sample4.jpeg";
 
+import c1 from "../../public/c1.jpeg";
+import c2 from "../../public/c2.jpeg";
+import c3 from "../../public/c3.jpeg";
+import c4 from "../../public/c4.jpeg";
+
 export const PROFILE = {
   name: "Haroun Kassouri",
   location: "Dublin, Ireland 🇮🇪",
@@ -38,4 +43,39 @@ export const SKILLS = [
   "Languages: HTML, CSS/TailwindCSS, JavaScript, Typescript",
   "TechnologiesNode.js, Django, Docker",
   "Databases: PostgreSQL, MySQL, MongoDB",
+];
+
+export const EXPERIENCES = [
+  {
+    img: c1,
+    year: "January 2024 - February 2024",
+    role: "Full Stack Software Engineer Intern",
+    company: "Department of Agriculture, Food and Marine..",
+    description: `Working on various projects including web applications and APIs using Angular, Typescript, and Node.js. Involved in both frontend and backend development, collaborating with team members to deliver high-quality software solutions.`,
+    technologies: ["Angular", "TypeScript", "Node.js", "MongoDB"],
+  },
+  {
+    img: c2,
+    year: "January 2024 - February 2024",
+    role: "Full Stack Software Engineer Intern",
+    company: "Department of Agriculture, Food and Marine..",
+    description: `Working on various projects including web applications and APIs using Angular, Typescript, and Node.js. Involved in both frontend and backend development, collaborating with team members to deliver high-quality software solutions.`,
+    technologies: ["Angular", "TypeScript", "Node.js", "MongoDB"],
+  },
+  {
+    img: c3,
+    year: "January 2024 - February 2024",
+    role: "Full Stack Software Engineer Intern",
+    company: "Department of Agriculture, Food and Marine..",
+    description: `Working on various projects including web applications and APIs using Angular, Typescript, and Node.js. Involved in both frontend and backend development, collaborating with team members to deliver high-quality software solutions.`,
+    technologies: ["Angular", "TypeScript", "Node.js", "MongoDB"],
+  },
+  {
+    img: c4,
+    year: "January 2024 - February 2024",
+    role: "Full Stack Software Engineer Intern",
+    company: "Department of Agriculture, Food and Marine..",
+    description: `Working on various projects including web applications and APIs using Angular, Typescript, and Node.js. Involved in both frontend and backend development, collaborating with team members to deliver high-quality software solutions.`,
+    technologies: ["Angular", "TypeScript", "Node.js", "MongoDB"],
+  },
 ];
