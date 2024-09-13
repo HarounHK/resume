db4bf8210b5b3f45c02fa0e6f5eb0d97702960ec
Haroun Kassouri <harounkassouri368@gmail.com>
2024-09-13 06:08:59 +0100

    Changed profile photo and shape

--- app/components/Homepge.jsx
+++ app/components/Homepge.jsx
@@ -7,11 +7,10 @@ export const Homepge = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center xl:justify-between xl:pt-8 xl:pb-24">
-          {/* {Text} */}
           <div className="text-center xl:text-left xl:pr-8">
             <span className="text-xl">Software Engineer</span>
             <h1 className="mb-6 text-5xl xl:text-[80px] leading-[1.1] font-semibold">
-              Hello I am <br />
+              Hello I'm <br />
               <span className="text-teal-400">Haroun Kassouri</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
@@ -46,10 +45,9 @@ export const Homepge = () => {
               </div>
             </div>
           </div>
-          {/* {Photo} */}
-            <div>
-                <Profile />
-            </div>
+          <div className="mt-8 xl:mt-0 xl:ml-12"> 
+            <Profile />
+          </div>
         </div>
       </div>
     </section>
--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -6,12 +6,18 @@ import Image from "next/image";
 export const Profile = () => {
   return (
     <div className="w-full h-full relative">
-        <motion.div>
-            <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mixble">
-                <Image src="/ProfilePic.jpeg" priority quality={100} fill
-                alt="" className="object-contain" />
-            </div>
-        </motion.div>
+      <motion.div>
+        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] rounded-full overflow-hidden bg-gradient-to-br from-slate-950 via-slate-950 to-transparent p-1">
+          <Image
+            src="/ProfilePicture.jpeg"
+            priority
+            quality={100}
+            fill
+            alt="Profile Picture"
+            className="object-cover rounded-full"
+          />
+        </div>
+      </motion.div>
     </div>
-)
-}
+  );
+};
