6be8d0d3359161adfcae5d2cf7089de3b02939ed
Haroun Kassouri <harounkassouri368@gmail.com>
2024-09-13 05:44:46 +0100

    Added basic photo temporary

--- app/components/Homepge.jsx
+++ app/components/Homepge.jsx
@@ -1,14 +1,15 @@
 import { FaDownload } from "react-icons/fa";
 import { SOCIALS } from "../constants/text"; // Make sure this path is correct
+import { Profile } from "./Profile";
 
 export const Homepge = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center xl:justify-between xl:pt-8 xl:pb-24">
           {/* {Text} */}
-          <div className="text-center xl:text-left">
-            <span className="text-xl">Software Developer</span>
+          <div className="text-center xl:text-left xl:pr-8">
+            <span className="text-xl">Software Engineer</span>
             <h1 className="mb-6 text-5xl xl:text-[80px] leading-[1.1] font-semibold">
               Hello I am <br />
               <span className="text-teal-400">Haroun Kassouri</span>
@@ -17,32 +18,38 @@ export const Homepge = () => {
               Full Stack Software Engineer with 3 years of hands-on programming experience.
               Proficient in Angular, React, and Next.js.
             </p>
-            <a
-              href="/CV.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              download
-              className="flex items-center justify-center mx-auto gap-x-2 rounded-full bg-slate-950 px-5 py-3 text-teal-400 text-sm font-semibold tracking-tight border-2 border-teal-400 hover:bg-teal-400 hover:text-black transition-colors"
-            >
-              Download CV
-              <FaDownload className="text-xl" />
-            </a>
-                <div className="mt-8 xl:mt-8 flex justify-center gap-8">
-                    {SOCIALS.map((item, index) => (
-                    <a
-                        key={index}
-                        href={item.path}
-                        className="text-white hover:text-teal-400 transition-colors"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {item.icon}
-                    </a>
-                    ))}
-                </div>
+            <div className="flex flex-col xl:flex-row items-center xl:items-start gap-4 xl:gap-8">
+              <a
+                href="/CV.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+                className="flex items-center justify-center mx-auto gap-x-2 rounded-full bg-slate-950 px-5 py-3 text-teal-400 text-sm font-semibold tracking-tight border-2 border-teal-400 hover:bg-teal-400 hover:text-black transition-colors"
+              >
+                Download CV
+                <FaDownload className="text-xl" />
+              </a>
+              <div className="mt-4 xl:mt-0 flex justify-center gap-6">
+                {SOCIALS.map((item, index) => (
+                  <a
+                    key={index}
+                    href={item.path}
+                    className="flex items-center justify-center w-12 h-12 rounded-full border-2 border-teal-400 text-teal-400 hover:bg-teal-400 hover:text-black transition-colors"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="text-xl">
+                      {item.icon}
+                    </div>
+                  </a>
+                ))}
+              </div>
+            </div>
           </div>
           {/* {Photo} */}
-          <div>photo</div>
+            <div>
+                <Profile />
+            </div>
         </div>
       </div>
     </section>
--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,43 +1,37 @@
-import React from 'react';
-
 export const NavBar = () => {
   return (
-    <div>
-      {/* <nav className='bg-teal-white border border-white'> */}
-      <nav>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-          <div className='flex items-center justify-between h-16'>
-            <div className='flex items-center'>
-              <div className='flex-shrink-0'>
-                <a href="/" className='text-white text-2xl font-bold'>
-                  HK<span className='text-teal-400'>.</span>
-                </a>
-              </div>
-            </div>
-            <div className='hidden md:block'>
-              <div className='ml-4 flex items-center space-x-4'>
-                <a href="/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Home
-                </a>
-                <a href="/projects" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Projects
-                </a>
-                <a href="/skills" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Skills
-                </a>
-                <a href="/workexperience" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>
-                  Work Experience
-                </a>
-              </div>
-            </div>
-            <div className='md:hidden flex items-center'>
-              <button className='inline-flex items-center justify-center p-2 rounded-md text-white md:text-white
-                hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-              </button>
+    <nav className='bg-slate-950 border-b border-teal-400'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center'>
+            <div className='flex-shrink-0'>
+              <a href="/" className='text-white text-2xl font-bold'>
+                HK<span className='text-teal-400'>.</span>
+              </a>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+
+          <div className='hidden md:flex flex-grow justify-end'>
+            <div className='ml-4 flex items-center space-x-4'>
+              <a href="/" className='text-white hover:bg-teal-400 hover:text-black rounded-lg p-2'>
+                Home
+              </a>
+              <a href="/projects" className='text-white hover:bg-teal-400 hover:text-black rounded-lg p-2'>
+                Projects
+              </a>
+              <a href="/skills" className='text-white hover:bg-teal-400 hover:text-black rounded-lg p-2'>
+                Skills
+              </a>
+              <a href="/workexperience" className='text-white hover:bg-teal-400 hover:text-black rounded-lg p-2'>
+                Work Experience
+              </a>
+            </div>
+          </div>
+
+          <div className='md:hidden flex items-center'>
+            <button className='inline-flex items-center justify-center p-2 rounded-md text-white hover:text-teal-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-400'>
+            </button>
+          </div>
+      </div>
+    </nav>
   );
-}
+};
